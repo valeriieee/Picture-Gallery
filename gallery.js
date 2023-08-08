45f471f24b938edb53131831a36880fcc5a777ba
valeriieee <chinelovalerie98@gmail.com>
2023-08-08 17:44:18 +0100

    Initial commit

---
+++ gallery.js
@@ -0,0 +1,41 @@
+const change1 = document.getElementById('change1');
+
+change1.addEventListener('click', (event) =>{
+    event.target.style.background = 'brown';
+    event.target.style.color = 'white';
+});
+
+const change2 = document.getElementById('change2');
+
+change2.addEventListener('click', (event) =>{
+    event.target.style.background = 'blue';
+    event.target.style.color = 'white';
+});
+
+const change3 = document.getElementById('change3');
+
+change3.addEventListener('click', (event) =>{
+    event.target.style.background = 'blueviolet';
+    event.target.style.color = 'white';
+});
+
+const change4 = document.getElementById('change4');
+
+change4.addEventListener('click', (event) =>{
+    event.target.style.background = 'slategrey';
+    event.target.style.color = 'white';
+});
+
+const change5 = document.getElementById('change5');
+
+change5.addEventListener('click', (event) =>{
+    event.target.style.background = 'green';
+    event.target.style.color = 'white';
+});
+
+const change6 = document.getElementById('change6');
+
+change6.addEventListener('click', (event) =>{
+    event.target.style.background = 'rgb(80, 25, 25)';
+    event.target.style.color = 'white';
+});
